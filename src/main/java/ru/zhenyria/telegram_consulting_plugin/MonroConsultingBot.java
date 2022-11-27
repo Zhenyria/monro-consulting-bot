e8cfc3c7c0db1956333598c93bd2f8b8cb55a0b6
Eugene Aribzhanov <catshaurma@gmail.com>
2022-11-27 22:04:32 +0600

    Implement inline menu

--- src/main/java/ru/zhenyria/telegram_consulting_plugin/MonroConsultingBot.java
+++ src/main/java/ru/zhenyria/telegram_consulting_plugin/MonroConsultingBot.java
@@ -5,17 +5,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.zhenyria.telegram_consulting_plugin.service.update_processor.MonroConsultingService;
+import ru.zhenyria.telegram_consulting_plugin.util.Command;
+import ru.zhenyria.telegram_consulting_plugin.util.CommandMapper;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class MonroConsultingBot extends TelegramLongPollingBot {
+
+    private final CommandMapper commandMapper;
 
     @Value("${telegram.botName}")
     private String botName;
@@ -45,5 +54,30 @@ public class MonroConsultingBot extends TelegramLongPollingBot {
                 log.warn(e.getMessage());
             }
         }
+    }
+
+    @Override
+    public void onRegister() {
+        try {
+            if (!super.execute(getCommands())) {
+                log.error("Bot commands list was not initialized");
+            }
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    /**
+     * Gets list of bot commands
+     *
+     * @return {@link SetMyCommands} instance which contains list of available commands
+     */
+    private SetMyCommands getCommands() {
+        List<BotCommand> botCommands = commandMapper.mapToBotCommand(
+                Arrays.stream(Command.values()).collect(Collectors.toList())
+        );
+        SetMyCommands myCommands = new SetMyCommands();
+        myCommands.setCommands(botCommands);
+        return myCommands;
     }
 }
